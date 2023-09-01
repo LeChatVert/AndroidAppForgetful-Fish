aabee3664f06cde4de39e6c9783ab3d05469ed6c
Racthor <kauffeisensylvain@gmail.com>
2023-09-01 20:12:10 +0200

    feat: update de la vue principale en utilisant les vues crees precedemment

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,28 +1,34 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <player-panel class="envers"></player-panel>
+      <menu-content></menu-content>
+      <player-panel></player-panel>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonButtons, IonToolbar, IonMenu, IonMenuButton } from '@ionic/vue';
+import PlayerPanel from './PlayerPanel.vue';
+import MenuContent from './MenuContent.vue';
+import { defineComponent } from 'vue';
+  
+export default defineComponent({
+  components: {
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonMenu,
+    IonMenuButton,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    PlayerPanel,
+    MenuContent,
+  }
+});
+
 </script>
 
 <style scoped>
